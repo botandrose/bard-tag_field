3c138ad3ef3797ab65aac30f19124cf48dec5422
Micah Geisel <micah@botandrose.com>
2025-12-23 16:44:28 -0600

    try this.

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,12 +12,20 @@ require_relative "test_app"
 require "bard/tag_field/cucumber"
 
 Capybara.register_driver :cuprite do |app|
-  Capybara::Cuprite::Driver.new(app,
+  options = {
     window_size: [1200, 800],
     headless: true,
     process_timeout: 30,
     timeout: 30
-  )
+  }
+
+  # Support custom browser path for CI (e.g., Playwright's chromium)
+  if ENV["BROWSER_PATH"]
+    browser_path = Dir.glob(ENV["BROWSER_PATH"]).first
+    options[:browser_path] = browser_path if browser_path
+  end
+
+  Capybara::Cuprite::Driver.new(app, **options)
 end
 
 Capybara.default_driver = :cuprite
